b242a72928defa0b4af1606b42a185391b4d0e83
David Gahnassia <davidgahnassia@gmail.com>
2020-01-27 16:45:43 +0200

    first user autocomplete

---
+++ frontend/src/app/components/user-info/user-info.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+
+@Component({
+  selector: 'app-user-info',
+  templateUrl: './user-info.component.html',
+  styleUrls: ['./user-info.component.scss']
+})
+export class UserInfoComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() user: User;
+
+  ngOnInit() {
+    console.log(this.user);
+
+  }
+
+}
